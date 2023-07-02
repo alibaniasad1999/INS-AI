f4f97a55cfeb67642b3af15031b77e1d1d4a895b
Ali <alibaniasad1999@yahoo.com>
2023-07-03 02:00:33 +0330

    add mozafari data and fix code and need optimization procces

--- Tunning/PSO_optimization.m
+++ Tunning/PSO_optimization.m
@@ -1,14 +1,33 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PSO optimization with saving history and plot %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fun = @fun_opt;
-nvars = 2; %%%%
-lb = [-5.12,-5.12]; %%%%%
-ub = [5.12,5.12]; %%%%%
-options = optimoptions('particleswarm','PlotFcn','pswplotbestf');
-[x,fval,exitflag,output] = particleswarm(fun,nvars,lb,ub,options);
+clear;
+clc;
 
-function y = fun_opt(x)
-    y = x.^2-10*x+20;
-    y = sum(sum(y));
-end
+%%
+nvars = 5;
+micro_g_to_meters_per_second_squared = 9.80665E-6;
+lb = zeros(1, nvars);
+ub = zeros(1, nvars);
+% Initial attitude uncertainty per axis (deg, converted to rad)
+lb(1) = deg2rad(0);
+ub(1) = deg2rad(2);
+% Initial velocity uncertainty per axis (m/s)
+lb(2) =0;
+ub(2) = 0.5;
+% Initial position uncertainty per axis (m)
+lb(3) =0;
+ub(3) = 20;
+% Initial accelerometer bias uncertainty per instrument (micro-g, converted
+% to m/s^2)
+lb(4) =0;
+ub(4) = 10000 * micro_g_to_meters_per_second_squared;
+% Initial gyro bias uncertainty per instrument (deg/hour, converted to rad/sec)
+lb(5) =0;
+ub(5) = 20 * pi/180 / 3600;
+
+%%
+
+options = optimoptions('particleswarm','PlotFcn','pswplotbestf');
+[x,fval,exitflag,output] = particleswarm(@Objective_function,nvars,...
+    lb,ub,options);
